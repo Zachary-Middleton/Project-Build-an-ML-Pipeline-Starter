5dd8e64854e553d94ce8f318b78c8f6d6ab4b3cf
zachary-middleton <zmidd15@wgu.edu>
2025-10-06 21:42:55 -0500

    Updated basic_cleaning, eda, and data_check

--- main.py
+++ main.py
@@ -52,19 +52,60 @@ def go(config: DictConfig):
 
         if "basic_cleaning" in active_steps:
             ##################
-            # Implement here #
+            # Get the absolute path to the directory where main.py lives
+            root_dir = os.path.dirname(os.path.abspath(__file__))
+            # Construct the absolute path to your component
+            component_path = os.path.join(root_dir, "src", "basic_cleaning")
+            _ = mlflow.run(
+                component_path,
+                "main",
+                # Parameters passed to the component's run.py script
+                parameters={
+                    "input_artifact": "sample.csv:latest",
+                    "output_artifact": config["etl"]["output_artifact"],
+                    "output_type": config["etl"]["output_type"],
+                    "output_description": config["etl"]["output_description"],
+                    "min_price": config["etl"]["min_price"],
+                    "max_price": config["etl"]["max_price"]
+                },
+            )
             ##################
             pass
 
         if "data_check" in active_steps:
             ##################
-            # Implement here #
+            ##################
+            # Get the absolute path to the directory where main.py lives
+            root_dir = os.path.dirname(os.path.abspath(__file__))
+            # Construct the absolute path to your component
+            component_path = os.path.join(root_dir, "src", "data_check")
+            _ = mlflow.run(
+                component_path,
+                "main",
+                # Parameters passed to the component's run.py script
+                parameters={
+                    "csv": "clean_sample.csv:latest",
+                    "ref": "clean_sample.csv:reference",
+                    "kl_threshold": config["data_check"]["kl_threshold"],
+                    "min_price": config["etl"]["min_price"],
+                    "max_price": config["etl"]["max_price"]
+                },
+            )
             ##################
             pass
 
         if "data_split" in active_steps:
             ##################
-            # Implement here #
+            _ = mlflow.run(
+                f"{config['main']['components_repository']}/train_val_test_split",
+                'main',
+                parameters={
+                    "input": "clean_sample.csv:latest",
+                    "test_size": config['modeling']['test_size'],
+                    "random_seed": config['modeling']['random_seed'],
+                    "stratify_by": config['modeling']['stratify_by']
+                }
+            )
             ##################
             pass
 
@@ -79,7 +120,23 @@ def go(config: DictConfig):
             # step
 
             ##################
-            # Implement here #
+            root_dir = os.path.dirname(os.path.abspath(__file__))
+            # Construct the absolute path to your component
+            component_path = os.path.join(root_dir, "src", "train_random_forest")
+            _ = mlflow.run(
+                component_path,
+                "main",
+                # Parameters passed to the component's run.py script
+                parameters={
+                    "trainval_artifact": "trainval_data.csv:latest",
+                    "val_size": config["modeling"]["val_size"],
+                    "random_seed": config["modeling"]["random_seed"],
+                    "stratify_by": config["modeling"]["stratify_by"],
+                    "rf_config": rf_config,
+                    "max_tfidf_features": config["modeling"]["max_tfidf_features"],
+                    "output_artifact": "random_forest_export"
+                },
+            )
             ##################
 
             pass
